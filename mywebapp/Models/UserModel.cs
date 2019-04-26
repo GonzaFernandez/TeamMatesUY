6fdb19f29ef2a7da8d8ae177781a9425b02aa5a5
GonzaloNicolari <49452968+GonzaloNicolari@users.noreply.github.com>
2019-04-26 15:41:21 -0300

    Update UserModel.cs

--- mywebapp/Models/UserModel.cs
+++ mywebapp/Models/UserModel.cs
@@ -3,10 +3,21 @@ using System;
 namespace mywebapp.Models{
     
     public class user{
+        [StringLength(20)]
+        [Required]
+        public string name {get; set;}.
         
-        public string name {get; set;}
-        public int age {get; set;}
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime birthday {get; set;}
+        
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$")]
         public string password  {get; set;}
+        
+        [Required]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")]
+        public string email {get; set;}
     }
         public user(){
         }
