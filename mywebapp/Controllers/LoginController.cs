33ba4f0e90cf492d060e3e38f16ae970ebc89a59
GonzaloNicolari <49452968+GonzaloNicolari@users.noreply.github.com>
2019-04-26 15:59:36 -0300

    Update LoginController.cs

--- mywebapp/Controllers/LoginController.cs
+++ mywebapp/Controllers/LoginController.cs
@@ -29,7 +29,19 @@ namespace mywebapp.Controllers
         // POST api/login
         [ActionName("enter")]
         [HttpPost("{id}")]
-        public void Post([FromBody] string value) {
+        public void Post(FormCollection values) {
+            var name = values["name"];
+            var password = values["pass"];
+            var age = 
+            var userList = new List<Student>{
+                new User(){name="Yhonatan", age="24", password="jhon456", email="eljhony@hotmail.com"},
+                new User(){name="Brayan", age="18", password="brayan456", email="elbrayan@gmail.com"},
+                new User(){name="Jesika", age="30", password="jesikca456", email="lajesika@tcs.com"},
+                new User(){name="Shamila", age="25", password="shamila456", email="lashamila@hotmail.com"},
+                new User(){name="Kevin", age="19", password="kevin456", email="elkevin@gmail.com"},
+                new User(){name="Maykol", age="21", password="maykol456", email="elmaykol@hotmail.com"},
+                new User(){name="Karlos", age="27", password="karlos456", email="elkarlos@hotmail.com"},
+            };
 
             return View("Home");
          }
